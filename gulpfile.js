da55f0212d7e7e61adf8e109109ee558cab9bfb7
Josef Kuchař <email@josefkuchar.cz>
2017-06-30 13:28:51 +0200

    Directory structure

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+
+gulp.task("babel", () => {
+    return gulp.src("src/index.js")
+        .pipe(babel())
+        .pipe(gulp.dest("bin"));
+});
+
+// Watch for changes
+gulp.task("watch", () => {
+    gulp.watch("src/**.js", ["babel"]);
+});
+
+// Run without parameters
+gulp.task("default", ["watch"]);
